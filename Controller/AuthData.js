32f47c5bbde21984ecdc47e9eb3ad11567c0acdf
Rakshith Acharya <rakshixh@gmail.com>
2024-04-25 16:47:34 +0530

    exported json data

---
+++ Controller/AuthData.js
@@ -0,0 +1,82 @@
+const authData = {
+  guests: [
+    {
+      username: "guest1",
+      password: "Guest1!",
+      email: "guest1@example.com",
+      role: "guest",
+    },
+    {
+      username: "guest2",
+      password: "Guest2!",
+      email: "guest2@example.com",
+      role: "guest",
+    },
+    {
+      username: "guest3",
+      password: "Guest3!",
+      email: "guest3@example.com",
+      role: "guest",
+    },
+    {
+      username: "guest4",
+      password: "Guest4!",
+      email: "guest4@example.com",
+      role: "guest",
+    },
+  ],
+  users: [
+    {
+      username: "user1",
+      password: "User1!",
+      email: "user1@example.com",
+      role: "user",
+    },
+    {
+      username: "user2",
+      password: "User2!",
+      email: "user2@example.com",
+      role: "user",
+    },
+    {
+      username: "user3",
+      password: "User3!",
+      email: "user3@example.com",
+      role: "user",
+    },
+    {
+      username: "user4",
+      password: "User4!",
+      email: "user4@example.com",
+      role: "user",
+    },
+  ],
+  admins: [
+    {
+      username: "admin1",
+      password: "Admin1!",
+      email: "admin1@example.com",
+      role: "admin",
+    },
+    {
+      username: "admin2",
+      password: "Admin2!",
+      email: "admin2@example.com",
+      role: "admin",
+    },
+    {
+      username: "admin3",
+      password: "Admin3!",
+      email: "admin3@example.com",
+      role: "admin",
+    },
+    {
+      username: "admin4",
+      password: "Admin4!",
+      email: "admin4@example.com",
+      role: "admin",
+    },
+  ],
+};
+
+module.exports = authData;
