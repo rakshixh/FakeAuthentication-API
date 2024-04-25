f557ecb922dc5be2570fc89cc2f699c5cf18b51c
Rakshith Acharya <rakshixh@gmail.com>
2024-04-25 11:33:51 +0530

    added favicon and initial route welcom message in index.js file

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const favicon = require("serve-favicon");
+const path = require("path");
+
+// create our express app
+const app = express();
+
+// middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Enable CORS for all routes
+app.use(cors());
+
+// Serve favicon
+app.use(favicon(path.join(__dirname, "favicon.ico")));
+
+// route
+app.get("/", (req, res) => {
+  res.send("Welcome to the Fake Authentication API!");
+});
+
+// start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
