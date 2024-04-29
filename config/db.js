9f2897840cd7ed95820d7f8aef7bd0e1b4067560
Rakshith Acharya <rakshixh@gmail.com>
2024-04-29 17:35:33 +0530

    added disconnectDB function to close the connection to DB

--- config/db.js
+++ config/db.js
@@ -1,14 +1,39 @@
 const mongoose = require("mongoose");
 
+let isConnected = false;
+
 const connectDB = async () => {
+  if (isConnected) {
+    console.log("Already connected to the database");
+    return;
+  }
+
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-
-    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+    console.log(
+      `MongoDB Connection Connected: ${conn.connection.host}`.cyan.underline
+    );
+    isConnected = true;
   } catch (error) {
-    console.log(error);
+    console.error(`Error connecting to database: ${error.message}`);
     process.exit(1);
   }
 };
 
-module.exports = connectDB;
+const disconnectDB = async () => {
+  if (!isConnected) {
+    console.log("Not connected to the database");
+    return;
+  }
+
+  try {
+    await mongoose.disconnect();
+    console.log("MongoDB Connection Disconnected".cyan.underline);
+    isConnected = false;
+  } catch (error) {
+    console.error(`Error disconnecting from database: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+module.exports = { connectDB, disconnectDB };
