f24cca58db00580d9107182237c53cba395a3de4
Rakshith Acharya <rakshixh@gmail.com>
2024-04-29 23:01:43 +0530

    added schedule once a 28 days to open and close the db connection

--- Controller/Controller.js
+++ Controller/Controller.js
@@ -1,7 +1,43 @@
 const express = require("express");
+const cron = require("node-cron");
 const apiRoutes = express.Router();
 const User = require("../models/staticUsers");
 const { connectDB, disconnectDB } = require("../config/db");
+
+// Function to make a connection to database and disconnect
+const OpenAndCloseConnection = async () => {
+  try {
+    await connectDB();
+    console.log("--------------------------------------------");
+    console.log("28 DAYS OVER!");
+    console.log(
+      "Making connection to the database to keep the cluster running!"
+    );
+    console.log("-------------------------------------------- \n \n");
+    setTimeout(async () => {
+      await disconnectDB();
+      console.log("--------------------------------------------");
+      console.log("20 Seconds Over!");
+      console.log("Disconnecting the connection from the database!");
+      console.log("-------------------------------------------- \n \n");
+    }, 20000); // Disconnect after 20 seconds
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
+
+// Schedule the function to run every 28 days
+cron.schedule(
+  "0 0 1 * *",
+  async () => {
+    // Runs at midnight on the 1st of every month
+    await OpenAndCloseConnection();
+  },
+  {
+    scheduled: true,
+    timezone: "Asia/Kolkata",
+  }
+);
 
 // Route to get all users data
 apiRoutes.get("/allusers", async (req, res) => {
