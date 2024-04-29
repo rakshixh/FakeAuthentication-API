46e886976c9b1b2234cf4e726c2434a18e4dd8aa
Rakshith Acharya <rakshixh@gmail.com>
2024-04-29 23:01:05 +0530

    beautified console logs from the db.js

--- config/db.js
+++ config/db.js
@@ -4,34 +4,40 @@ let isConnected = false;
 
 const connectDB = async () => {
   if (isConnected) {
-    console.log("Already connected to the database");
+    console.log("[ Already connected to the database! ]".magenta.bold);
     return;
   }
 
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(
-      `MongoDB Connection Connected: ${conn.connection.host}`.cyan.underline
+      `\n \n[ Application connected to MongoDB Database ]`.magenta.bold
     );
     isConnected = true;
   } catch (error) {
-    console.error(`Error connecting to database: ${error.message}`);
+    console.error(
+      `Error connecting to database: ${error.message}`.red.italic.underline
+    );
     process.exit(1);
   }
 };
 
 const disconnectDB = async () => {
   if (!isConnected) {
-    console.log("Not connected to the database");
+    console.log("[ Not connected to the database! ]".red.bold);
     return;
   }
 
   try {
     await mongoose.disconnect();
-    console.log("MongoDB Connection Disconnected".cyan.underline);
+    console.log(
+      "\n \n[ Application disconnected from MongoDB Database ]".red.bold
+    );
     isConnected = false;
   } catch (error) {
-    console.error(`Error disconnecting from database: ${error.message}`);
+    console.error(
+      `Error disconnecting from database: ${error.message}`.red.italic.underline
+    );
     process.exit(1);
   }
 };
