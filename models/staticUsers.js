a37127a6daef7df74924f201fb280d27687d057d
Rakshith Acharya <rakshixh@gmail.com>
2024-04-29 16:11:36 +0530

    created mongoose "User" schema.

---
+++ models/staticUsers.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    role: {
+      type: String,
+      enum: ["guest", "user", "admin"],
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    address: {
+      street: {
+        type: String,
+        required: true,
+      },
+      city: {
+        type: String,
+        required: true,
+      },
+      state: {
+        type: String,
+        required: true,
+      },
+      zipcode: {
+        type: String,
+        required: true,
+      },
+    },
+  },
+  { collection: "staticUsers" } // Define collection name here
+);
+
+module.exports = mongoose.model("User", userSchema);
