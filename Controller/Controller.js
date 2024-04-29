b6e0e936a06518a6bbc85771a1abcc4d1efed910
Rakshith Acharya <rakshixh@gmail.com>
2024-04-29 16:18:20 +0530

    converted, allusers, users, guests, admins, login api to use mongodb using mongoose

--- Controller/Controller.js
+++ Controller/Controller.js
@@ -1,32 +1,11 @@
 const express = require("express");
 const apiRoutes = express.Router();
+const User = require("../models/staticUsers");
 
-// Import JSON data from Data.js initially
-let jsonData = require("./AuthData");
-
-// Function to update jsonData with fresh data from Data.js
-const updateData = () => {
+// Route to get all users using Mongoose
+apiRoutes.get("/allusers", async (req, res) => {
   try {
-    delete require.cache[require.resolve("./AuthData")]; // Clear cache to force re-import
-    jsonData = require("./AuthData");
-    console.log("Data updated at: ", new Date());
-  } catch (error) {
-    console.error("Error updating data:", error);
-  }
-};
-
-// Call updateData initially and then every 60 minutes
-updateData(); // Initial update
-const interval = setInterval(updateData, 60 * 60 * 1000);
-
-// Route to get all users
-apiRoutes.get("/allusers", (req, res) => {
-  try {
-    const allUsers = [].concat(
-      jsonData.guests,
-      jsonData.users,
-      jsonData.admins
-    );
+    const allUsers = await User.find();
     const response = {
       statusCode: 200,
       status: true,
@@ -44,13 +23,14 @@ apiRoutes.get("/allusers", (req, res) => {
 });
 
 // Route to get only Guest Users
-apiRoutes.get("/guests", (req, res) => {
+apiRoutes.get("/guests", async (req, res) => {
   try {
+    const guests = await User.find({ role: "guest" });
     const response = {
       statusCode: 200,
       status: true,
       message: "All guest users fetched successfully",
-      guests: jsonData.guests,
+      guests: guests,
     };
     res.status(200).json(response);
   } catch (error) {
@@ -62,33 +42,35 @@ apiRoutes.get("/guests", (req, res) => {
   }
 });
 
-// Route to get only normal Users
-apiRoutes.get("/users", (req, res) => {
+// Route to get only Admins Users
+apiRoutes.get("/admins", async (req, res) => {
   try {
-    const response = {
-      statusCode: 200,
-      status: true,
-      message: "All normal users fetched successfully",
-      normalUsers: jsonData.users,
-    };
-    res.status(200).json(response);
-  } catch (error) {
-    res.status(200).json({
-      statusCode: 500,
-      status: false,
-      message: "Internal server error",
-    });
-  }
-});
-
-// Route to get only Admin Users
-apiRoutes.get("/admins", (req, res) => {
-  try {
+    const admins = await User.find({ role: "admin" });
     const response = {
       statusCode: 200,
       status: true,
       message: "All admin users fetched successfully",
-      admins: jsonData.admins,
+      guests: admins,
+    };
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(200).json({
+      statusCode: 500,
+      status: false,
+      message: "Internal server error",
+    });
+  }
+});
+
+// Route to get only Normal Users
+apiRoutes.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({ role: "user" });
+    const response = {
+      statusCode: 200,
+      status: true,
+      message: "All normal users fetched successfully",
+      guests: users,
     };
     res.status(200).json(response);
   } catch (error) {
@@ -101,28 +83,21 @@ apiRoutes.get("/admins", (req, res) => {
 });
 
 // Route to login the users
-apiRoutes.post("/login", (req, res) => {
+apiRoutes.post("/login", async (req, res) => {
   try {
     const { username, password } = req.body;
-    // Find the user
-    let user = jsonData.guests.find((user) => user.username === username);
-    if (!user) {
-      user = jsonData.users.find((user) => user.username === username);
-      if (!user) {
-        user = jsonData.admins.find((user) => user.username === username);
-      }
-    }
+    const user = await User.findOne({ username: username });
 
+    // If user not found or password doesn't match, send error response
     if (!user || user.password !== password) {
-      res.status(200).json({
+      return res.status(200).json({
         statusCode: 401,
         status: false,
         message: "Invalid username or password",
       });
-      return;
     }
 
-    // User found and password matched
+    // User found and password matched, send success response
     res.status(200).json({
       statusCode: 200,
       status: true,
@@ -130,7 +105,9 @@ apiRoutes.post("/login", (req, res) => {
       user: {
         username: user.username,
         email: user.email,
+        name: user.name,
         role: user.role,
+        address: user.address,
       },
     });
   } catch (error) {
