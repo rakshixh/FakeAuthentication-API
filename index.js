3acb975c2e9799ba4d383072adf7c4b40a2db522
Rakshith Acharya <rakshixh@gmail.com>
2024-04-30 14:17:03 +0530

    swifted the cron schedule task of running function to make connection to db to index.js

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const favicon = require("serve-favicon");
 const path = require("path");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
+const cron = require("node-cron");
+const { connectDB, disconnectDB } = require("./config/db");
 
 // create our express app
 const app = express();
@@ -18,6 +20,41 @@ app.use(cors());
 
 // Serve favicon
 app.use(favicon(path.join(__dirname, "favicon.ico")));
+
+// Function to make a connection to database and disconnect
+const OpenAndCloseConnection = async () => {
+  try {
+    await connectDB();
+    console.log("--------------------------------------------");
+    console.log("28 DAYS OVER!");
+    console.log(
+      "Making connection to the database to keep the cluster running!"
+    );
+    console.log("-------------------------------------------- \n \n");
+    setTimeout(async () => {
+      await disconnectDB();
+      console.log("--------------------------------------------");
+      console.log("20 Seconds Over!");
+      console.log("Disconnecting the connection from the database!");
+      console.log("-------------------------------------------- \n \n");
+    }, 20000); // Disconnect after 20 seconds
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
+
+// Schedule the function to run every 28 days
+cron.schedule(
+  "0 0 1 * *",
+  async () => {
+    // Runs at midnight on the 1st of every month
+    await OpenAndCloseConnection();
+  },
+  {
+    scheduled: true,
+    timezone: "Asia/Kolkata",
+  }
+);
 
 // route
 app.get("/", (req, res) => {
