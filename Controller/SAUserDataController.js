f46ce72da8149b93c1b969dd5f4e147c366c121f
Rakshith Acharya <rakshixh@gmail.com>
2024-05-01 18:53:46 +0530

    added update password api with validation and check for current password of the users under the super admins.

--- Controller/SAUserDataController.js
+++ Controller/SAUserDataController.js
@@ -15,7 +15,7 @@ SAUsersDataRoutes.put(
     await connectDB();
     try {
       const { SuperAdminUserName, username } = req.params;
-      const { name, address, role } = req.body;
+      const { name, email, address, role } = req.body;
 
       // Check if the user exists and was created by the specified Super Admin
       const user = await DynamicUsersData.findOne({
@@ -24,7 +24,7 @@ SAUsersDataRoutes.put(
       });
       if (!user) {
         disconnectDB();
-        return res.status(404).json({
+        return res.status(200).json({
           statusCode: 404,
           status: false,
           message: "User not found or not created by the specified Super Admin",
@@ -33,7 +33,8 @@ SAUsersDataRoutes.put(
 
       // Update user data
       if (name) user.name = name;
-      if (address) user.address = address; // Update entire address object
+      if (email) user.email = email;
+      if (address) user.address = address; //address is an object with other properties
       if (role) user.role = role;
 
       // Save the updated user
@@ -51,7 +52,87 @@ SAUsersDataRoutes.put(
       });
       disconnectDB();
     } catch (error) {
-      res.status(500).json({
+      res.status(200).json({
+        statusCode: 500,
+        status: false,
+        message: "Internal server error",
+      });
+    }
+  }
+);
+
+//Route to update the password of the user created by the super admin
+SAUsersDataRoutes.put(
+  "/update/password/:SuperAdminUserName/:username",
+  async (req, res) => {
+    await connectDB();
+    try {
+      const { SuperAdminUserName, username } = req.params;
+      const { password, rePassword, currentPassword } = req.body;
+
+      // Check if the user exists and was created by the specified Super Admin
+      const user = await DynamicUsersData.findOne({
+        username,
+        SuperAdminUserName,
+      });
+      if (!user) {
+        disconnectDB();
+        return res.status(200).json({
+          statusCode: 404,
+          status: false,
+          message: "User not found or not created by the specified Super Admin",
+        });
+      }
+
+      // Verify current password
+      if (user.password !== currentPassword) {
+        disconnectDB();
+        return res.status(200).json({
+          statusCode: 400,
+          status: false,
+          message: "Current password is incorrect",
+        });
+      }
+
+      // Check if the new password and confirm password match
+      if (password !== rePassword) {
+        disconnectDB();
+        return res.status(200).json({
+          statusCode: 400,
+          status: false,
+          message: "Passwords do not match",
+        });
+      }
+
+      // Check if the new password is the same as the current password
+      if (password === currentPassword) {
+        disconnectDB();
+        return res.status(200).json({
+          statusCode: 400,
+          status: false,
+          message: "New password must be different from the current password",
+        });
+      }
+
+      // Update user password
+      user.password = password;
+
+      // Save the updated user
+      await user.save();
+      await SuperAdmin.findOneAndUpdate(
+        { SuperAdminUserName },
+        { lastAccessed: getCurrentDateTimeIndia() }
+      );
+
+      res.status(200).json({
+        statusCode: 200,
+        status: true,
+        message: "User password updated successfully",
+        user: user,
+      });
+      disconnectDB();
+    } catch (error) {
+      res.status(200).json({
         statusCode: 500,
         status: false,
         message: "Internal server error",
