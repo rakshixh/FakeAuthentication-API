eb94c599071369bfd8dc04c0549ff8cc845a75d2
Rakshith Acharya <rakshixh@gmail.com>
2024-05-01 14:19:33 +0530

    added routes of SAUsersDataRoutes to Routes file

--- Routes/Routes.js
+++ Routes/Routes.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 const userRoutes = require("../Controller/UserController");
 const superAdminRoutes = require("../Controller/SuperAdminController");
+const SAUsersDataRoutes = require("../Controller/SAUserDataController");
 
 router.use("/staticUsers", userRoutes);
-router.use("/dynamicUsers", superAdminRoutes);
+router.use("/dynamicUsers", superAdminRoutes, SAUsersDataRoutes);
 module.exports = router;
