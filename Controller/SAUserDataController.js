280cc54e9d5d343f2894711775737dc26ba49cb0
Rakshith Acharya <rakshixh@gmail.com>
2024-05-01 18:58:03 +0530

    added api to delete the user created by the Super Admin

--- Controller/SAUserDataController.js
+++ Controller/SAUserDataController.js
@@ -141,4 +141,52 @@ SAUsersDataRoutes.put(
   }
 );
 
+// Route to DELETE the user created by the Super Admin
+SAUsersDataRoutes.delete(
+  "/delete/user/:SuperAdminUserName/:username",
+  async (req, res) => {
+    await connectDB();
+    try {
+      const { SuperAdminUserName, username } = req.params;
+
+      // Check if the user exists and was created by the specified Super Admin
+      const user = await DynamicUsersData.findOne({
+        username,
+        SuperAdminUserName,
+      });
+      if (!user) {
+        disconnectDB();
+        return res.status(200).json({
+          statusCode: 404,
+          status: false,
+          message: "User not found or not created by the specified Super Admin",
+        });
+      }
+
+      // Delete the user
+      await DynamicUsersData.findOneAndDelete({
+        username,
+        SuperAdminUserName,
+      });
+      await SuperAdmin.findOneAndUpdate(
+        { SuperAdminUserName },
+        { lastAccessed: getCurrentDateTimeIndia() }
+      );
+
+      res.status(200).json({
+        statusCode: 200,
+        status: true,
+        message: "User deleted successfully",
+      });
+      disconnectDB();
+    } catch (error) {
+      res.status(200).json({
+        statusCode: 500,
+        status: false,
+        message: "Internal server error",
+      });
+    }
+  }
+);
+
 module.exports = SAUsersDataRoutes;
