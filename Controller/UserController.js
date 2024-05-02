9eeb47ea4110094c0505f88586cee1cf9fd5a9d5
Rakshith Acharya <rakshixh@gmail.com>
2024-05-02 23:21:25 +0530

    added swagger annotations for UserController.js

--- Controller/UserController.js
+++ Controller/UserController.js
@@ -6,6 +6,45 @@ const { connectDB, disconnectDB } = require("../config/db");
 // @desc These are Static APIs with only GET requests
 // ----------------------------------------------------------------
 
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ *   description: User management APIs
+ */
+
+/**
+ * @swagger
+ * /api/staticUsers/allusers:
+ *   get:
+ *     summary: Get all users
+ *     tags: [Users]
+ *     description: Retrieve all users from the database
+ *     responses:
+ *       '200':
+ *         description: A JSON array of all users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 statusCode:
+ *                   type: integer
+ *                   example: 200
+ *                 status:
+ *                   type: boolean
+ *                   example: true
+ *                 message:
+ *                   type: string
+ *                   example: All users fetched successfully
+ *                 allUsers:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/User'
+ *       '500':
+ *         description: Internal server error
+ */
+
 // Route to get all users data
 userRoutes.get("/allusers", async (req, res) => {
   await connectDB();
@@ -27,6 +66,38 @@ userRoutes.get("/allusers", async (req, res) => {
     });
   }
 });
+
+/**
+ * @swagger
+ * /api/guests:
+ *   get:
+ *     summary: Get all guest users
+ *     tags: [Users]
+ *     description: Retrieve all guest users from the database
+ *     responses:
+ *       '200':
+ *         description: A JSON array of all guest users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 statusCode:
+ *                   type: integer
+ *                   example: 200
+ *                 status:
+ *                   type: boolean
+ *                   example: true
+ *                 message:
+ *                   type: string
+ *                   example: All guest users fetched successfully
+ *                 guests:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/User'
+ *       '500':
+ *         description: Internal server error
+ */
 
 // Route to get only Guest Users
 userRoutes.get("/guests", async (req, res) => {
@@ -50,6 +121,38 @@ userRoutes.get("/guests", async (req, res) => {
   }
 });
 
+/**
+ * @swagger
+ * /api/admins:
+ *   get:
+ *     summary: Get all admin users
+ *     tags: [Users]
+ *     description: Retrieve all admin users from the database
+ *     responses:
+ *       '200':
+ *         description: A JSON array of all admin users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 statusCode:
+ *                   type: integer
+ *                   example: 200
+ *                 status:
+ *                   type: boolean
+ *                   example: true
+ *                 message:
+ *                   type: string
+ *                   example: All admin users fetched successfully
+ *                 admins:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/User'
+ *       '500':
+ *         description: Internal server error
+ */
+
 // Route to get only Admins Users
 userRoutes.get("/admins", async (req, res) => {
   await connectDB();
@@ -72,6 +175,38 @@ userRoutes.get("/admins", async (req, res) => {
   }
 });
 
+/**
+ * @swagger
+ * /api/users:
+ *   get:
+ *     summary: Get all normal users
+ *     tags: [Users]
+ *     description: Retrieve all normal users from the database
+ *     responses:
+ *       '200':
+ *         description: A JSON array of all normal users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 statusCode:
+ *                   type: integer
+ *                   example: 200
+ *                 status:
+ *                   type: boolean
+ *                   example: true
+ *                 message:
+ *                   type: string
+ *                   example: All normal users fetched successfully
+ *                 users:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/User'
+ *       '500':
+ *         description: Internal server error
+ */
+
 // Route to get only Normal Users
 userRoutes.get("/users", async (req, res) => {
   await connectDB();
@@ -93,6 +228,79 @@ userRoutes.get("/users", async (req, res) => {
     });
   }
 });
+
+/**
+ * @swagger
+ * /api/login:
+ *   post:
+ *     summary: Log in user
+ *     tags: [Users]
+ *     description: Log in the user with username and password
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *                 example: johndoe
+ *               password:
+ *                 type: string
+ *                 example: password123
+ *     responses:
+ *       '200':
+ *         description: Login successful
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 statusCode:
+ *                   type: integer
+ *                   example: 200
+ *                 status:
+ *                   type: boolean
+ *                   example: true
+ *                 message:
+ *                   type: string
+ *                   example: Login successful
+ *                 user:
+ *                   $ref: '#/components/schemas/User'
+ *       '401':
+ *         description: Invalid username or password
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 statusCode:
+ *                   type: integer
+ *                   example: 401
+ *                 status:
+ *                   type: boolean
+ *                   example: false
+ *                 message:
+ *                   type: string
+ *                   example: Invalid username or password
+ *       '500':
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 statusCode:
+ *                   type: integer
+ *                   example: 500
+ *                 status:
+ *                   type: boolean
+ *                   example: false
+ *                 message:
+ *                   type: string
+ *                   example: Internal server error
+ */
 
 // Route to login the users
 userRoutes.post("/login", async (req, res) => {
