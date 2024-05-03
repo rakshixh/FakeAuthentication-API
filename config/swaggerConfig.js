182448198a6218b56896b0e33123cd29bedd847b
Rakshith Acharya <rakshixh@gmail.com>
2024-05-04 00:29:17 +0530

    added path for api.yaml file in swaggerConfig.js

--- config/swaggerConfig.js
+++ config/swaggerConfig.js
@@ -1,6 +1,12 @@
 const swaggerUI = require("swagger-ui-express");
 const YAML = require("yamljs");
-const swaggerJSDocs = YAML.load("api.yaml");
+const path = require("path");
+
+// Define the full path to the api.yaml file
+const apiSpecPath = path.resolve(__dirname, "../api.yaml");
+
+// Load the YAML file
+const swaggerJSDocs = YAML.load(apiSpecPath);
 
 const options = {
   customCss: ` img {content:url(\'../logo.svg\'); height:auto;} `,
