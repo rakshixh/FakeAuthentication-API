b61552a081035a8aa570ab50a80e9f4026b93c73
Rakshith Acharya <rakshixh@gmail.com>
2024-05-04 21:51:15 +0530

    updated swaggeConfig.js

--- config/swaggerConfig.js
+++ config/swaggerConfig.js
@@ -1,24 +1,63 @@
-const swaggerUI = require("swagger-ui-express");
-const YAML = require("yamljs");
-const path = require("path");
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
-// Define the full path to the api.yaml file
-const apiSpecPath = path.resolve(__dirname, "../api.yaml");
+// Define base URLs for both localhost and hosted server
+const PORT = process.env.PORT || 5000;
+const localhostURL = `http://localhost:${PORT}`;
+const hostedURL = "https://fakeauthentication-api.toystack.dev/";
 
-// Load the YAML file
-const swaggerJSDocs = YAML.load(apiSpecPath);
-
-// const variable for CSS URL
-const CSS_URL =
-  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
-
-const options = {
-  customCss: ` img {content:url(\'../logo.svg\'); height:auto;} `,
-  customfavIcon: "../favicon.ico",
-  customSiteTitle: "custom api",
+// Swagger definition for OAS 3
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Fake Authentication API",
+      description: "APIs for user authentication",
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        url: localhostURL,
+        description: "Local Server",
+      },
+      {
+        url: hostedURL,
+        description: "Hosted Server",
+      },
+    ],
+    components: {
+      schemas: {
+        User: {
+          type: "object",
+          properties: {
+            username: { type: "string" },
+            password: { type: "string" },
+            email: { type: "string" },
+            role: { type: "string" },
+            name: { type: "string" },
+            address: {
+              type: "object",
+              properties: {
+                street: { type: "string" },
+                city: { type: "string" },
+                state: { type: "string" },
+                zipcode: { type: "string" },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  apis: ["./Controller/*.js"],
 };
 
+// Swagger serve and setup
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+const swaggerServe = swaggerUi.serve;
+const swaggerSetup = swaggerUi.setup(swaggerDocs);
+
 module.exports = {
-  swaggerServe: swaggerUI.serve,
-  swaggerSetup: swaggerUI.setup(swaggerJSDocs, { customCssUrl: CSS_URL }),
+  swaggerServe,
+  swaggerSetup,
 };
