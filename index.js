2786e577f6a564d262803fb7e72aea2e0932caf9
Rakshith Acharya <rakshixh@gmail.com>
2024-05-04 21:50:51 +0530

    updated index.js file with swagger config

--- index.js
+++ index.js
@@ -7,19 +7,10 @@ const colors = require("colors");
 const dotenv = require("dotenv").config();
 const cron = require("node-cron");
 const { connectDB, disconnectDB } = require("./config/db");
-
 const { swaggerServe, swaggerSetup } = require("./config/swaggerConfig");
-const swaggerJsDoc = require("swagger-jsdoc");
-const swaggerUi = require("swagger-ui-express");
 
 // Define base URLs for both localhost and hosted server
 const PORT = process.env.PORT || 5000;
-const localhostURL = `http://localhost:${PORT}`;
-const hostedURL = "https://fakeauthentication-api.vercel.app/";
-
-// const variable for CSS URL
-const CSS_URL =
-  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
 
 // create our express app
 const app = express();
@@ -33,52 +24,6 @@ app.use(cors());
 
 // Serve favicon
 app.use(favicon(path.join(__dirname, "favicon.ico")));
-
-// Swagger definition for OAS 3
-const swaggerOptions = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Fake Authentication API",
-      description: "APIs for user authentication",
-      version: "1.0.0",
-    },
-    servers: [
-      {
-        url: localhostURL,
-        description: "Local Server",
-      },
-      {
-        url: hostedURL,
-        description: "Hosted Server",
-      },
-    ],
-    components: {
-      schemas: {
-        User: {
-          type: "object",
-          properties: {
-            username: { type: "string" },
-            password: { type: "string" },
-            email: { type: "string" },
-            role: { type: "string" },
-            name: { type: "string" },
-            address: {
-              type: "object",
-              properties: {
-                street: { type: "string" },
-                city: { type: "string" },
-                state: { type: "string" },
-                zipcode: { type: "string" },
-              },
-            },
-          },
-        },
-      },
-    },
-  },
-  apis: ["./Controller/*.js"],
-};
 
 // Function to make a connection to database and disconnect
 const OpenAndCloseConnection = async () => {
@@ -124,13 +69,6 @@ const routes = require("./Routes/Routes");
 app.use("/api", routes);
 
 //Swagger serve route
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
-// app.use(
-//   "/api/api-docs",
-//   swaggerUi.serve,
-//   swaggerUi.setup(swaggerDocs, { customCssUrl: CSS_URL })
-// );
-
 app.use("/api/api-docs", swaggerServe, swaggerSetup);
 
 // start the server
