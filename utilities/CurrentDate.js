f8f02f07585b65632c8b9c92cffae84f42bbefe2
Rakshith Acharya <rakshixh@gmail.com>
2024-05-17 15:38:01 +0530

    updated date and time file

--- utilities/CurrentDate.js
+++ utilities/CurrentDate.js
@@ -1,22 +1,24 @@
-exports.getCurrentDateTimeIndia = function () {
-  const ISTOffset = 330 * 60 * 1000; // UTC+5:30 in minutes
-  const currentDate = new Date(Date.now() + ISTOffset);
-
-  // Adjusting time by subtracting 5 hours and 30 minutes.
-  const adjustedDate = new Date(currentDate.getTime() - (5 * 60 + 30) * 60000);
-
-  // Extracting date components.
-  const year = adjustedDate.getFullYear();
-  const month = String(adjustedDate.getMonth() + 1).padStart(2, "0");
-  const day = String(adjustedDate.getDate()).padStart(2, "0");
-
-  // Extracting time components.
-  let hours = adjustedDate.getHours();
-  const minutes = String(adjustedDate.getMinutes()).padStart(2, "0");
+function getCurrentDateTime() {
+  const date = new Date(); // Date object
+  const currentDate = date.toISOString().slice(0, 10); // Current date
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
   const ampm = hours >= 12 ? "PM" : "AM";
-  hours = hours % 12;
-  hours = hours ? hours : 12; // Convert 0 to 12 for 12-hour clock
+  const formattedHours = hours % 12 || 12;
+  const currentTime =
+    formattedHours +
+    ":" +
+    (minutes < 10 ? "0" : "") +
+    minutes +
+    ":" +
+    (seconds < 10 ? "0" : "") +
+    seconds +
+    " " +
+    ampm; // Current time with AM or PM
+  return currentDate + " " + currentTime;
+}
 
-  // Constructing the formatted string.
-  return `${year}-${month}-${day} ${hours}:${minutes} ${ampm}`;
+module.exports = {
+  getCurrentDateTime,
 };
