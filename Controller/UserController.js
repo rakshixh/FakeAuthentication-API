6b84c925c1449c5917cc0400500f099b44faedde
Rakshith Acharya <rakshixh@gmail.com>
2024-05-19 20:41:32 +0530

    added swagger annotation for 500 internal server error for get all admins api

--- Controller/UserController.js
+++ Controller/UserController.js
@@ -179,6 +179,20 @@ userRoutes.get("/guests", async (req, res) => {
  *                     $ref: '#/components/schemas/User'
  *       '500':
  *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 statusCode:
+ *                   type: integer
+ *                   example: 500
+ *                 status:
+ *                   type: boolean
+ *                   example: false
+ *                 message:
+ *                   type: string
+ *                   example: Internal server error
  */
 
 // Route to get only Admins Users
